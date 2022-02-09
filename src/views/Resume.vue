90dd1bea1f04f2f0f5d581678012e758c69c904f
lifechoices <mkhosanasiyabonga@gmail.com>
2022-02-09 09:22:02 +0200

    make it better

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,25 +1,42 @@
 <template>
   <section class="single-section alt-bg CV-area" id="CV-area">
-
-  <div class="container">                      
-    <div class="row text-center justify-content-center mb-5">
+    <div class="container">
+      <div class="row text-center justify-content-center mb-5">
         <div class="col-xl-6 col-lg-8">
-            <h1 class="text-uppercase font-weight-bolder" style="box-shadow: inset 0 -6px 0 #F5A281; color:white; margin-bottom: 30px;">Resume</h1>
-            <h5 class="p-tag1">I am very proud of the path I have taken. Explore the history that made me the professional that I am today.</h5>
+          <h1
+            class="text-uppercase font-weight-bolder"
+            style="
+              box-shadow: inset 0 -6px 0 #f5a281;
+              color: white;
+              margin-bottom: 30px;">
+            Resume
+          </h1>
+          <h5 class="p-tag1">
+            I am very proud of the path I have taken. Explore the history that
+            made me the professional that I am today.
+          </h5>
         </div>
-    </div>
+      </div>
 
-    <div class="row">
+      <div class="row">
         <div class="col">
-            <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
-                <div class="timeline-step">
-                    <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2003">
-                        <div class="inner-circle"></div>
-                        <p class="h6 mt-3 mb-1">2010-2013</p>
-                        <p class="p-tag mb-0 mb-lg-0">Peak View Secondary School</p>
-                    </div>
-                </div>
-                <div class="timeline-step">
+          <div class="timeline-steps aos-init aos-animate" data-aos="fade-up">
+            <div
+              class="timeline-step">
+              <div
+                class="timeline-content"
+                data-toggle="popover"
+                data-trigger="hover"
+                data-placement="top"
+                title=""
+                data-content="And here's some amazing content. It's very engaging. Right?"
+                data-original-title="2003">
+                <div class="inner-circle" v-for="(resume, index) in Resumes" :key="index">
+                <p class="h6 mt-3 mb-1">{{ resume.year }}</p>
+                <p class="p-tag mb-0 mb-lg-0">{{ resume.institution }}</p>
+              </div>
+            </div>
+            <!-- <div class="timeline-step">
                     <div class="timeline-content" data-toggle="popover" data-trigger="hover" data-placement="top" title="" data-content="And here's some amazing content. It's very engaging. Right?" data-original-title="2004">
                         <div class="inner-circle"></div>
                         <p class="h6 mt-3 mb-1">July 2014-Jan 2015</p>
@@ -46,12 +63,13 @@
                         <p class="h6 mt-3 mb-1">Sep 2021-</p>
                         <p class="p-tag mb-0 mb-lg-0">Life Choice Coding Academy</p>
                     </div>
-                </div>
+                </div> -->
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-  <!-- <div class="Resume" id="Resume">
+    <!-- <div class="Resume" id="Resume">
     <div data-aos="flip-left"
     data-aos-easing="ease-out-cubic"
     data-aos-duration="2000">  
@@ -106,96 +124,133 @@
     </div>
 </div>
 </div> -->
-<div class="bts text-uppercase" style="text-align: center; margin-top: 50px">
-  <a href="https://resume-example.com/builder/resume/02c62779-dcaa-41f7-8009-9ee21b644039/preview" target="blank" class="btn extra" style="border-color: blue; background-color: #F5A281; font-weight: bold;">Download Resume</a>
-</div>
-<!-- </div> -->
-</section>
-
+    <div
+      class="bts text-uppercase"
+      style="text-align: center; margin-top: 50px"
+    >
+      <a
+        href="https://resume-example.com/builder/resume/02c62779-dcaa-41f7-8009-9ee21b644039/preview"
+        target="blank"
+        class="btn extra"
+        style="border-color: blue; background-color: #f5a281; font-weight: bold"
+        >Download Resume</a
+      >
+    </div>
+    <!-- </div> -->
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      Resumes: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:5000/Resumes")
+      .then((response) => {
+        console.log("hey");
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.Resumes = data;
+      });
+  },
+};
 </script>
 
 <style>
-.container{margin-top: 150px;}
+.container {
+  margin-top: 150px;
+}
 
-body{margin-top:20px;}
+body {
+  margin-top: 20px;
+}
 
 .timeline-steps {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 
 .timeline-steps .timeline-step {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    margin: 1rem
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin: 1rem;
 }
 
 .font-weight-bold {
-    margin-top: 60px;
+  margin-top: 60px;
 }
 
-@media (min-width:768px) {
-    .timeline-steps .timeline-step:not(:last-child):after {
-        content: "";
-        display: block;
-        border-top: .25rem dotted #3b82f6;
-        width: 3.46rem;
-        position: absolute;
-        left: 7.5rem;
-        top: .3125rem
-    }
-    .timeline-steps .timeline-step:not(:first-child):before {
-        content: "";
-        display: block;
-        border-top: .25rem dotted #3b82f6;
-        width: 3.8125rem;
-        position: absolute;
-        right: 7.5rem;
-        top: .3125rem
-    }
+@media (min-width: 768px) {
+  .timeline-steps .timeline-step:not(:last-child):after {
+    content: "";
+    display: block;
+    border-top: 0.25rem dotted #3b82f6;
+    width: 3.46rem;
+    position: absolute;
+    left: 7.5rem;
+    top: 0.3125rem;
+  }
+  .timeline-steps .timeline-step:not(:first-child):before {
+    content: "";
+    display: block;
+    border-top: 0.25rem dotted #3b82f6;
+    width: 3.8125rem;
+    position: absolute;
+    right: 7.5rem;
+    top: 0.3125rem;
+  }
 }
 
 .timeline-steps .timeline-content {
-    width: 10rem;
-    text-align: center
+  width: 10rem;
+  text-align: center;
 }
 
 .timeline-steps .timeline-content .inner-circle {
-    border-radius: 1.5rem;
-    height: 1rem;
-    width: 1rem;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #3b82f6
+  border-radius: 1.5rem;
+  height: 1rem;
+  width: 1rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #3b82f6;
 }
 
 .timeline-steps .timeline-content .inner-circle:before {
-    content: "";
-    background-color: #3b82f6;
-    display: inline-block;
-    height: 3rem;
-    width: 3rem;
-    min-width: 3rem;
-    border-radius: 6.25rem;
-    opacity: .5
+  content: "";
+  background-color: #3b82f6;
+  display: inline-block;
+  height: 3rem;
+  width: 3rem;
+  min-width: 3rem;
+  border-radius: 6.25rem;
+  opacity: 0.5;
 }
-.Resume{
-    height: 100vh;
-    width: cover;
-    padding: 60px 0 40px;
-    z-index: 9478470375037507094755085370953;
+.Resume {
+  height: 100vh;
+  width: cover;
+  padding: 60px 0 40px;
+  z-index: 9478470375037507094755085370953;
 }
-.p-tag1{color: white; font-weight: bolder;}
-.p-tag{color: white; font-weight: bold;}
-.single-section{background-color: gray; height: 100vh; overflow-x: auto;}
+.p-tag1 {
+  color: white;
+  font-weight: bolder;
+}
+.p-tag {
+  color: white;
+  font-weight: bold;
+}
+.single-section {
+  background-color: gray;
+  height: 100vh;
+  overflow-x: auto;
+}
 </style>
